34d7d80c6266a4f7a909822193b20048ab6dadc4
Molik Singla <moliksingla02@gmail.com>
2022-03-12 12:23:24 +0530

    Added API Link

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,5 +1,6 @@
 // Socket io -------------------------------
-const socket = io("http://localhost:8000");
+// const socket = io("http://localhost:8000");
+const socket = io("https://instachatclone.herokuapp.com");
 
 socket.on("recieve-message", message => {
     if (firstCome) {
